794d3d6b12eded05ba3370bbabd254d007debfd3
Nestorvalencia17 <nestor46170@gmail.com>
2025-01-27 16:44:11 -0300

    pruebas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -47,28 +47,43 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       body: Container(
-        color: const Color(0xFFF5F5F5),
-        height: height,
-        width: width,
-        child: Column(
-          children: [
-            widget_search_home(), // Widget de búsqueda.
-            const SizedBox(height: 10),
-            Container(
-              height: 130,
-              width: 330,
-              color: Colors.white, // Placeholder para otro contenido.
-            ),
-            const SizedBox(height: 10),
-            Categorias(
-              categories: _categories,
-            ),
-          ],
+  color: const Color(0xFFF5F5F5),
+  height: height,
+  width: width,
+  child: Column(
+    children: [
+      widget_search_home(), // Widget de búsqueda.
+      const SizedBox(height: 10),
+      Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 130,
+                width: 330,
+                color: Colors.white, // Placeholder para otro contenido.
+              ),
+              const SizedBox(height: 10),
+              Categorias(
+                categories: _categories,
+              ),
+              SizedBox(height: 20,),
+              Productos(
+                categories: _categories,
+              ),
+               SizedBox(height: 20,),
+            ],
+          ),
         ),
       ),
+    ],
+  ),
+)
     );
   }
 }
+
+
 
 class Categorias extends StatelessWidget {
   final List categories; // Lista de categorías.
@@ -157,7 +172,7 @@ class Categorias extends StatelessWidget {
                                 color: Colors.black.withOpacity(0.05),
                               )
                             ],
-                            borderRadius: BorderRadius.circular(100),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                           child: Image.network(
                             'https://distribuidoraassefperico.com.ar${categoria['imagen']}', // Concatenar correctamente la URL base con la ruta de la imagen
@@ -189,5 +204,123 @@ class Categorias extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Productos extends StatelessWidget {
+  final List categories; // Lista de categorías.
+
+  const Productos({
+    super.key,
+    required this.categories,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Limita las categorías a un máximo de 10.
+    final displayedCategories = categories.take(8).toList();
+
+    return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8),
+    child: Container(
+      
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border.all(
+              color: const Color.fromARGB(255, 202, 202, 202), width: 0.5),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 3,
+              spreadRadius: 1,
+            )
+          ]),
+      child: Column(
+        children: [
+          
+          Wrap(
+            children: List.generate(
+              categories.take(16).length, // Toma 4 productos aleatorios
+              (index) {
+                final categoria = categories[index];
+
+                return GestureDetector(
+                  onTap: () {
+                  },
+                  child: Container(
+                    width: 170,
+                    height: 240, // 2 elementos por fila
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(0),
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 202, 202, 202),
+                          width: 0.2),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 8,right: 8),
+                          child: Container(
+                            child: Image.network(
+                            'https://distribuidoraassefperico.com.ar${categoria['imagen']}', // Concatenar correctamente la URL base con la ruta de la imagen
+                            fit: BoxFit.cover,
+                            
+                          ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 5, top: 5, right: 5),
+                          child: Container(
+                            width: double.infinity,
+                            child: Text(
+                              categoria['nombre'] ?? 'Sin nombre',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Text(
+                                  'USD \$${categoria['price'].toString()}',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          
+        ],
+      ),
+    ),
+  );
   }
 }
